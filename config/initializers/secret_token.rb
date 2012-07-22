92f9d9f3feb3ccc6561fa73ed9bd33bc03d2cc1b
Jeff Montgomery <poolstud1970@hotmail.com>
2012-07-22 19:17:17 -0400

    Initial checkin of new repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApache2::Application.config.secret_token = '8f741c31bd15f146885936ed8b99d65be460b4534bc90ddd2fb888c5d522af78c5db38cf52954ecd1ba85b3ed594246f452c238432d59b3cd15352716b90449b'
